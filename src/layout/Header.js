763be7d3f83e8f8b71f4d1eb0e0aa775ea84612c
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-07-30 02:05:01 +0600

    💅🛠️🏷️added and completed

---
+++ src/layout/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/layout/header.scss";
+
+export default function Header() {
+  return (
+    <header className="app_header">
+      <img src="/palo-logo.svg" alt="prothom alo logo" />
+    </header>
+  );
+}
