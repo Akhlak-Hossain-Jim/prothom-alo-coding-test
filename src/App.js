39d0ef400c8bb7b0ca50cadefcad90b97033e5a2
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-07-30 18:39:51 +0600

    🛠️updated & completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/app.scss";
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./layout/Header";
+import Home from "./pages/Home";
+import NewsPage from "./pages/NewsPage";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news/:id" element={<NewsPage />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
