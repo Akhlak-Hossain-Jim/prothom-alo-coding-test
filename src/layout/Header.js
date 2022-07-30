cf905ef4f42bccbcfe12a1075dd70bb8e32eb5b3
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-07-30 18:37:34 +0600

    🪲bug fix

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,11 @@ import "../styles/layout/header.scss";
 export default function Header() {
   return (
     <header className="app_header">
-      <img src="/palo-logo.svg" alt="prothom alo logo" />
+      <img
+        src="/palo-logo.svg"
+        alt="prothom alo logo"
+        onClick={() => window.open("/", "_self")}
+      />
     </header>
   );
 }
